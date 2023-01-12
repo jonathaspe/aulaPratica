3f171274bd739a4ba6f38ded96f691d0e24f16a1
Jonathas Santos <jonathaspe@hotmail.com>
2023-01-11 22:50:03 -0300

    Commit final da aula de hoje

--- src/main/java/br/com/jonathaspe/Main.java
+++ src/main/java/br/com/jonathaspe/Main.java
@@ -1,4 +1,6 @@
-package br.com.jonathaspe;public class Main {
+package br.com.jonathaspe;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
--- src/test/java/br/com/jonathaspe/CalculadoraTest.java
+++ src/test/java/br/com/jonathaspe/CalculadoraTest.java
@@ -1,2 +1,19 @@
-package br.com.jonathaspe;public class CalculadoraTest {
+package br.com.jonathaspe;
+
+public class CalculadoraTest {
+    public static void main(String[] args){
+        CalculadoraTest calculadora = new CalculadoraTest();
+
+        System.out.println(retornarNumeroDez());
+        System.out.println(somarDoisNumeros(20, 30));
+        System.out.println(calculadora.somarDoisNumeros(23, 25));
+    }
+
+    public static int retornarNumeroDez(){
+        return 10;
+    }
+
+    public static int somarDoisNumeros(int primeiroNumero, int segundoNumero){
+        return primeiroNumero + segundoNumero;
+    }
 }
